ec876a6a1906ae2feeaa4df1158027fe99603650
archimage-wiz <archimage@yandex.ru>
2024-02-07 20:41:29 +0300

    cosmetics
    modified:   event-object/dropdown/task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,10 +1,18 @@
-
+/** @type {HTMLElement} */
 const dropdownButton = document.querySelector(".dropdown__value");
+/** @type {HTMLElement} */
 const dropdownList = document.querySelector(".dropdown__list");
 
-dropdownButton.addEventListener("click", () => dropdownList.classList.toggle("dropdown__list_active"));
+dropdownButton.addEventListener("click", () =>
+    dropdownList.classList.toggle("dropdown__list_active")
+);
 dropdownList.addEventListener("click", listSelect);
 
+/**
+ * Listens for a click event and updates the dropdown button and list accordingly.
+ * @param {Event} event - The event object
+ * @return {void}
+ */
 function listSelect(event) {
     event.preventDefault();
     dropdownButton.textContent = event.target.textContent;
